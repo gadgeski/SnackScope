7f1630e4b39e0c354b82359ff13ed7dcb28b6cd0
gadgeski <xtwh.ce5bil@gmail.com>
2025-01-17 15:27:20 +0900

    Initial Commit

---
+++ SnackScope/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnackScope
+//
+//  Created by Dev Tech on 2025/01/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnackScope/SnackScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackScopeApp.swift
+//  SnackScope
+//
+//  Created by Dev Tech on 2025/01/17.
+//
+
+import SwiftUI
+
+@main
+struct SnackScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
